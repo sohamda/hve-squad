04c927e7bcefc9751b5e0121dbb637f0624e08b2
sohamda <iam.soham@gmail.com>
2026-06-18 11:19:57 +0200

    removed hve-core

--- hve-core/.github/skills/hve-core/vally-tests/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: MIT
-"""Tests for the Vally corpus importer."""
--- hve-core/.github/skills/project-planning/adr-author/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: MIT
-"""Test package marker for the ADR Author skill."""
